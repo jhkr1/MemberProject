f06ab96bd632c11d689fa5eab3c1dedf13390ee4
Jihun <wlgjs06061@naver.com>
2023-07-16 19:54:29 +0900

    "Feat: 전체 회원 조회"

--- MemberProject/src/main/java/com/jhkr1/MemberProject/controller/MemberController.java
+++ MemberProject/src/main/java/com/jhkr1/MemberProject/controller/MemberController.java
@@ -4,11 +4,13 @@ import com.jhkr1.MemberProject.dto.MemberDTO;
 import com.jhkr1.MemberProject.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -18,32 +20,41 @@ public class MemberController {
 
     // 회원가입 페이지 출력 요청
     @GetMapping("/member/save")
-    public String saveForm(){
+    public String saveForm() {
         return "save";
     }
 
     @PostMapping("/member/save")
-    public String save(@ModelAttribute MemberDTO memberDTO){
+    public String save(@ModelAttribute MemberDTO memberDTO) {
         System.out.println("MemberController.save");
         System.out.println("memberDTO = " + memberDTO);
         memberService.save(memberDTO);
         return "login";
     }
+
     @GetMapping("/member/login")
-    public String loginForm(){
+    public String loginForm() {
         return "login";
     }
 
     @PostMapping("/member/login")
-    public String login(@ModelAttribute MemberDTO memberDTO, HttpSession session){
+    public String login(@ModelAttribute MemberDTO memberDTO, HttpSession session) {
         MemberDTO loginResult = memberService.login(memberDTO);
-        if(loginResult != null){
+        if (loginResult != null) {
             // login 성공
             session.setAttribute("loginEmail", loginResult.getMemberEmail());
             return "main";
-        }else {
+        } else {
             // login 실패
             return "login";
         }
+    }
+
+    @GetMapping("/member/")
+    public String findAll(Model model){
+        List<MemberDTO> memberDTOList = memberService.findAll();
+        // 어떤 html로 가져갈 데이터가 있다면 model을 사용
+        model.addAttribute("memberList", memberDTOList);
+        return "list";
     }
 }
